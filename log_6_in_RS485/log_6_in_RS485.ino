9df76c645490284c000f12eab634232b03f96ce3
ronm <ronmaca1@gmail.com>
2018-11-06 13:51:39 -0600

    added rtc, rs485 direction control, heartbeat pin.

--- log_6_in_RS485/log_6_in_RS485.ino
+++ log_6_in_RS485/log_6_in_RS485.ino
@@ -1,10 +1,17 @@
 //<debuuging defines>
 #define   _DEBUG_ // for serial debugging
 //#undef    _DEBUG_
-
 //</debuging defines>
 
+#define RXEN            LOW
+#define RXDIS           HIGH
+#define TXEN            HIGH
+#define TXDIS           LOW
+
 //<pin defines>
+#define RS485RXEN       2 // LOW to enable
+#define RS485TXEN       3 // HIGH to enable
+#define DEBUG_HEARTBEAT 9 // tracking millis() accuracy
 #define B1OXYGEN        A0
 #define B2OXYGEN        A3
 #define NOTUSED         A2
@@ -19,11 +26,17 @@
 #define RED_ON          32
 #define GREEN_ON        127
 //</led defines>
-
+#include <Wire.h>
+#include <stdio.h>
+#include <PCF8583.h>
 #include <SPI.h>
 #include <SD.h>
+
+PCF8583 myrtc (0xA0);
 unsigned long startmillis = 0;
 unsigned long currentmillis = 0;
+unsigned long seconds = 0;
+unsigned char loopcount = 0;
 
 /*
 long mymap(long x, long in_min, long in_max, long out_min, long out_max)
@@ -40,18 +53,24 @@ void setup() {
   // per datasheet page 43
   DIDR0 |= _BV(ADC3D) | _BV(ADC2D) | _BV(ADC1D) | _BV(ADC0D); //adc0 through 3
 
-  // <set all unused pins to INPUT_PULLUP >  
+  // <set all unused pins to OUTPUT >  
   // digital pins
-  int i;
-  const int numunused =6;  
-  uint8_t unused_pins[numunused]{2,3,4,9,A4,A5};
+  int i;  
+  const unsigned char unused_pins[1]{9};
+  const size_t numunused = sizeof(unused_pins)/sizeof(unused_pins[0]);
+
     for(i=0;i<numunused;i++){
-      digitalWrite(unused_pins[i],HIGH);
-      pinMode(unused_pins[i],INPUT_PULLUP);
+      digitalWrite(unused_pins[i],LOW);
+      pinMode(unused_pins[i],OUTPUT);
       }
   // </set all unused pins to output and LOW>
 
   // <digital pins in use>
+    
+    
+    pinMode (DEBUG_HEARTBEAT,OUTPUT);
+    pinMode (RS485TXEN,OUTPUT);
+    pinMode (RS485RXEN,OUTPUT);
     pinMode(HEAT_1,INPUT);
     pinMode(HEAT_2,INPUT);
   // </digital pins in use>
@@ -63,6 +82,9 @@ void setup() {
 
   //delay(20); // a short delay to let things stabilize
   #ifdef  _DEBUG_
+  
+  digitalWrite(RS485TXEN,TXEN);
+  digitalWrite(RS485TXEN,RXDIS);
   Serial.begin(57600);
   Serial.print("Initializing SD card...");
   #endif
@@ -74,26 +96,63 @@ void setup() {
     #endif
     digitalWrite(ERRORLED_GREEN,OFF); // no card inserted
     digitalWrite(ERRORLED_RED,RED_ON);
-    while (1);
+    while (1); // hang till power down and card inserted
     
   }
   #ifdef _DEBUG_
   Serial.println("card initialized.");
   #endif
+  String  StartString = "";
+  StartString += "Startup";
   digitalWrite(ERRORLED_RED,OFF); // all is well, get on with it
   digitalWrite(ERRORLED_GREEN,GREEN_ON);
+
+
+  myrtc.get_time();
+  char time[24];
+  sprintf(time, "%02d/%02d/%02d %02d:%02d:%02d",
+  myrtc.year, myrtc.month, myrtc.day, myrtc.hour, myrtc.minute, myrtc.second);
+  Serial.println(time);
+  StartString += time;
+  digitalWrite(DEBUG_HEARTBEAT,LOW);
   
   startmillis = millis();
+ 
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 int notused, tpos, b1oxygen, b2oxygen, temp;
+
+      // Use of (byte) type casting and ascii math to achieve result.  
+     if(Serial.available() > 0){
+       myrtc.year= (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48)) + 2000;
+       myrtc.month = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+       myrtc.day = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+       myrtc.hour  = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+       myrtc.minute = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+       myrtc.second = (byte) ((Serial.read() - 48) * 10 + (Serial.read() - 48)); 
+
+       if(Serial.read() == ';'){
+         Serial.println("setting date");
+         myrtc.set_time();
+       }
+    }
+
 
 String dataString = "";
 currentmillis = millis();
-
+if(loopcount == 0){
+  myrtc.get_time();
+  char time[24];
+  sprintf(time, "%02d/%02d/%02d %02d:%02d:%02d",
+  myrtc.year, myrtc.month, myrtc.day, myrtc.hour, myrtc.minute, myrtc.second);
+  
+  dataString += String(time);
+  dataString += ";\r\n";
+  }
 dataString += String(millis()-startmillis);
 dataString += String(",");
 
@@ -164,7 +223,7 @@ dataString += String(",");
  
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  File dataFile = SD.open("datalog.csv", FILE_WRITE);
 
   // if the file is available, write to it:
   if (dataFile) {
@@ -178,7 +237,7 @@ dataString += String(",");
   // if the file isn't open, pop up an error:
   else {
     #ifdef _DEBUG_
-    Serial.println("error opening datalog.txt");
+    Serial.println("error opening datalog.csv");
     #endif
     analogWrite (ERRORLED_GREEN,GREEN_ON); // yellow if file error
     analogWrite (ERRORLED_RED,RED_ON);
@@ -198,6 +257,14 @@ dataString += String(",");
     delay (100);
   #endif  
   */
+
 while (millis()-currentmillis < 100); // do every 100 millis aka 10 sample / sec.
-  
+ 
+if(loopcount <=600){
+  loopcount++; 
+  }else {
+    loopcount = 0; //reset each minute 
+  // see beginning of loop for the usage of this}
+  }
+digitalWrite(DEBUG_HEARTBEAT,!digitalRead(DEBUG_HEARTBEAT));
 }
